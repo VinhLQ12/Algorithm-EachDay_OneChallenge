02a3effe80161ba3cbf2bb7850ef455dcf4b5c2d
Vinhlam2512 <lamvinh2431@gmail.com>
2022-04-06 16:04:40 +0700

    Day6

--- CodeSignal/commonCharacterCount.js
+++ CodeSignal/commonCharacterCount.js
@@ -1,18 +1,10 @@
 var s1 = 'aabcc';
 var s2 = 'adcaa';
 
-function solution(s1, s2) {
-    var sum = 0;
-    for(let a in s1) {
-        for(let b in s2){
-            if(s1[a] === s2[b]) {
-                console.log(s1[a], s2[b]);
-                sum++;
-                break;
-            }
-        }
-    }
-    return sum;
+function min(s1, s2) {
+    return s1 < s2 ? s1 : s2;
 }
+
+function solution(s1, s2) {}
 
 console.log(solution(s1, s2));
