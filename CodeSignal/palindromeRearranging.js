c74835b0bf9ba92040e3941cd15e37b505eed663
Vinhlam2512 <lamvinh2431@gmail.com>
2022-04-20 22:37:45 +0700

    Day14

--- CodeSignal/palindromeRearranging.js
+++ CodeSignal/palindromeRearranging.js
@@ -1,5 +1,31 @@
-var s = 'abca';
+var s = 'abdhuierf';
 var a = {};
-var newS = s.split('');
+function countChar(arr, s) {
+    return arr.filter((element) => element === s).length;
+}
 
-function solution(s) {}
+function arrCount(s) {
+    var check = [];
+    var arrChar = s.split('');
+    var arr = Array.from(new Set(s));
+    for (let i of arr) {
+        check.push(countChar(arrChar, i));
+    }
+    return check;
+}
+console.log(arrCount(s));
+function solution(s) {
+    var check = arrCount(s);
+    var len = check.length;
+    if (len == 1) return true;
+    var count = 0;
+    for (let i of check) {
+        if (i % 2 == 1) {
+            count++;
+        }
+    }
+
+    return count <= 1;
+}
+
+console.log(solution(s));
